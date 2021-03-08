3c605bfa1e6df46ddce965010066321d1e48f3d0
LiangGuo <leonguo08@gmail.com>
2021-03-08 10:31:24 +0800

    ver1.0 overall

--- backend/main.py
+++ backend/main.py
@@ -4,20 +4,27 @@ server side  of TMDB for CSCI571 HW06
 """
 
 import requests
+from datetime import datetime
 from flask.globals import request
 from flask import jsonify
 from flask import Flask, json
 from requests.models import PreparedRequest
+from werkzeug.datastructures import iter_multi_items
+
 KEY = "bec75727adfa221f20fb0a25788e805c"
 BASE = "https://api.themoviedb.org"
-IMG_PR = "https://image.tmdb.org/t/p/w780"
-genre = {10759: 'Action & Adventure', 16: 'Animation', 35: 'Comedy', 80: 'Crime', 99: 'Documentary', 18: 'Drama',
-         10751: 'Family', 10762: 'Kids', 9648: 'Mystery',
-         10763: 'News', 10764: 'Reality', 10765: 'Sci-Fi & Fantasy', 
-         10766: 'Soap', 10767: 'Talk', 10768: 'War & Politics',
-         37: 'Western', 28: 'Action', 12: 'Adventure', 14: 'Fantasy', 36: 'History', 27: 'Horror',
-         10402: 'Music', 10749: 'Romance', 878: 'Science Fiction', 10770: 'TV Movie', 53: 'Thriller', 10752: 'War'}
+IMG_PR = "https://image.tmdb.org/t/p/"
 app = Flask(__name__, static_folder="../front", static_url_path='')
+
+genre = {10759: 'Action & Adventure', 16: 'Animation', 35: 'Comedy',
+         80: 'Crime', 99: 'Documentary', 18: 'Drama',
+         10751: 'Family', 10762: 'Kids', 9648: 'Mystery',
+         10763: 'News', 10764: 'Reality', 10765: 'Sci-Fi & Fantasy',
+         10766: 'Soap', 10767: 'Talk', 10768: 'War & Politics',
+         37: 'Western', 28: 'Action', 12: 'Adventure', 14: 'Fantasy',
+         36: 'History', 27: 'Horror',
+         10402: 'Music', 10749: 'Romance', 878: 'Science Fiction',
+         10770: 'TV Movie', 53: 'Thriller', 10752: 'War'}
 
 
 @app.route("/")
@@ -32,7 +39,8 @@ def fetch_trending():
     return json 
     """
     url = f"{BASE}/3/trending/movie/week?api_key={KEY}"
-    data = requests.get(url, verify=False).json()  # TODO error handle here
+    data = fetch(url)
+
     resp = []
     for item in data.get("results", [])[:5]:
         path = item.get("backdrop_path")
@@ -41,7 +49,7 @@ def fetch_trending():
         resp.append(
             {
                 "title": item.get("title"),
-                "backdrop_path": IMG_PR + path,
+                "backdrop_path": IMG_PR+"w780" + path if path else "statics/movie-placeholder.jpg",
                 "release_date": item.get("release_date")
             }
         )
@@ -51,7 +59,7 @@ def fetch_trending():
 @app.route("/airtoday")
 def fetch_airtoday():
     url = f"{BASE}/3/tv/airing_today?api_key={KEY}"
-    data = requests.get(url, verify=False).json()  # TODO error handle here
+    data = fetch(url)
     resp = []
     for item in data.get("results", [])[:5]:
         path = item.get("backdrop_path")
@@ -60,7 +68,7 @@ def fetch_airtoday():
         resp.append(
             {
                 "name": item.get("name"),
-                "backdrop_path": IMG_PR+path if path else "",
+                "backdrop_path": IMG_PR+"w780"+path if path else "",
                 "first_air_date": item.get("first_air_date")
             }
         )
@@ -71,8 +79,6 @@ mov_fields = ["id", "title", "overview", "poster_path",
               "release_date", "vote_average", "vote_count",
               "genre_ids"]
 # tv_fields has "name""first_air_date"
-
-
 @app.route("/search")
 def search():
     """
@@ -81,7 +87,7 @@ def search():
     kv = request.args.get("kv")
     type = request.args.get("type")
     url = f"{BASE}/3/search/{type}?api_key={KEY}&query={kv}&language=en-US&page=1&include_adult=false"
-    data = requests.get(url, verify=False).json()
+    data = fetch(url)
     res = data.get("results", [])
     resp = []
     for item in res:
@@ -93,16 +99,17 @@ def search():
             one["title"] = item.get("name", "")
             one["release_date"] = item.get("first_air_date", "")
             one["type"] = 1
-        elif (item.get("media_type") == "movie") or(type=="movie"):
+        elif (item.get("media_type") == "movie") or(type == "movie"):
             one["type"] = 2
-        one["genres"] =[genre[gen_id] for gen_id in one["genre_ids"]]
-        
+        one["genres"] = [genre[gen_id] for gen_id in one["genre_ids"]]
+
         if not one["poster_path"]:
             one["poster_path"] = "statics/movie_poster.png"
         else:
-            one["poster_path"] = "https://image.tmdb.org/t/p/w185"+one["poster_path"]
-        
-        for k,val in one.items():
+            one["poster_path"] = "https://image.tmdb.org/t/p/w185" + \
+                one["poster_path"]
+
+        for k, val in one.items():
             if not val:
                 one[k] = "N/A"
 
@@ -110,26 +117,98 @@ def search():
     return jsonify(resp)
 
 
-@app.route("/detail")
-def detail():
-
+@app.route("/media_basic")
+def media_basic():
     """fetch deatil info of tv/movies 
     params: type: movie,tv ; media_id
     """
-    # basic info 
+    media_id = request.args.get("media_id")
+    type = request.args.get("type")
+    url = f"{BASE}/3/{type}/{media_id}?api_key={KEY}&language=en-US"
+    
+    item = fetch(url)
 
-    # cast
+    one = {}
+    # deal with img
+    if item["backdrop_path"]:
+        one["backdrop_path"] = IMG_PR+"w780" + item["backdrop_path"]
+    else:
+        one["backdrop_path"] = "statics/movie-placeholder.jpg"
+    #  diff key name between tv and movie
+    if type == "tv":
+        one["name"] = item.get("name")
+        one["year"] = item.get("first_air_date")
+    else:
+        one["name"] = item.get("title")
+        one["year"] = item.get("release_date")
+    one["year"] = one["year"][:4] if one["year"] else ""
+    # genres
+    one["genres"] = [gen.get("name") for gen in item.get("genres", [])]
+    # spoken_languages
+    one["spoken_languages"] = [lag.get("english_name")
+                               for lag in item.get("spoken_languages", [])]
+    # vote_average
+    one["vote_average"] = item.get(
+        "vote_average")/2 if item.get("vote_average") else "NA"
+    # vote_count
+    one["vote_count"] = item.get("vote_count")
+    # overview
+    one["overview"] = item.get("overview")
+    return jsonify(one)
 
-    # review
-    pass
+@app.route("/media_cast")
+def fetch_cast():
+    media_id = request.args.get("media_id")
+    type = request.args.get("type")
+    url = f"{BASE}/3/{type}/{media_id}/credits?api_key={KEY}&language=en-US"
+    data = fetch(url)
+    resp = [] 
+    for item in data.get("cast",[])[:8]:
+        one = {}
+        one["name"] = item.get("name")
+        path = item.get("profile_path") 
+        one["profile_path"] = IMG_PR+"w185"+path if path else "statics/person-placeholder.png"
+        one["character"] = item.get("character")
+        resp.append(one)
+    return jsonify(resp)
 
-def fetch_cast(type,media_id,data):
-    pass 
+@app.route("/media_review")
+def fetch_review():
+    media_id = request.args.get("media_id")
+    type = request.args.get("type")
+    url = f"{BASE}/3/{type}/{media_id}/reviews?api_key={KEY}&language=en-US"
+    data = fetch(url).get("results",[])
+    resp = [] 
+    
+    for item in data[:5]:
+        one = {} 
+        one["username"] = item.get("author_details",{}).get("username","")
+        one["content"] = item.get("content")
+        rating = item.get("author_details",{}).get("rating")
+        one["rating"] = rating/2 if rating else -1
+        raw_time = item.get("created_at","")[:10]
+        print(raw_time)
+        if len(raw_time)==10:
+            one["created_at"] = f"{raw_time[5:7]}/{raw_time[8:10]}/{raw_time[0:4]}"
+        else:
+            one["created_at"] = ""
+        resp.append(one)
+    return jsonify(resp)
 
-def fetch_review(type,media_id,data):
-    pass 
 
-
+def fetch(url):
+    retry_time = 0
+    while retry_time < 3:
+        try:
+            data = requests.get(url, verify=False)
+            js_data = data.json()
+            return js_data
+        except requests.exceptions.SSLError as e:
+            print("ssl error")
+            retry_time += 1
+        except Exception:
+            retry_time += 1
+    return None
 
 
 def fetch_genre():
@@ -143,55 +222,11 @@ def fetch_genre():
         genre[item["id"]] = item["name"]
     print(genre)
 
+
 def replace_gen(ids):
     return [genre[gen_id] for gen_id in ids]
+
 
 if __name__ == "__main__":
     # fetch_genre()
     app.run(debug=True)
-
-
-"""
-basic
-v1. id - the id of the movie.
-2. title - the title of the movie.
-3. runtime - The runtime of the movie.
-4. release_date - the date the movie was released.
-v5. spoken_languages - different audio languages the movie is available in.
-v6. vote_average - the average of ratings given by reviewers.
-v7. vote_count - total number of reviews received by the movie.
-v8. poster_path - path for the image of the poster.
-v9. backdrop_path - path for the larger backdrop image.
-v10. genres - genres of the movie.
-
-
-- 2. episode_run_time - duration of each episode.
-- 3. first_air_date - first air date of the show
-- 6. name - the name of the tv show.
-没用到 7. number_of_seasons - number of seasons the tv show was aired for.
-8. overview - the synopsis of the tv show.
-
-
-https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
-https://api.themoviedb.org/3/tv/{tv_show_id}?api_key=<<api_key>>&language=en-
-US
-
-
-cast all
-1. name - Name of the actor
-2. profile_path - Path for the image of the actor
-3. character - The character played by the actor.
-https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>&language=en-US
-
-
-https://api.themoviedb.org/3/tv/{tv_show_id}/credits?api_key=<<api_key>>&language=en-US
-
-review 
-1. username - The username of the reviewer, inside the author_details object.
-2. content - The content of the review
-3. rating - The rating given by the reviewer for the movie, inside the author details object.
-4. created_at - The date the review was created.
-https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=<<api_key>>&language=en- US&page=1
-https://api.themoviedb.org/3/tv/{tv_show_id}/reviews?api_key=<<api_key>>&language=en- US&page=1
-
-"""
--- front/main.js
+++ front/main.js
@@ -1,83 +1,95 @@
-window.onload=handleTrending()
+window.onload = handleTrending()
 
 // fetch and change poster of home page 
-async function handleTrending(){
-    var data = await getJSON("/trending")
-    var dataAir = await getJSON("/airtoday")
-    // trending movies
-    var imgtre = document.getElementById("imgtre")
-    var introtre = document.getElementById("introtre")
-    // air today
-    var imgair = document.getElementById("imgair")
-    var introair = document.getElementById("introair")
-    var index = 0
-    const change =()=>{
-        var item = data[index]
-        imgtre.style.backgroundImage= "url("+item["backdrop_path"]+")"
-        introtre.innerHTML=item["title"] +"("+item["release_date"].substring(0,4)+")"
-        
-        var item2 = dataAir[index]
-        imgair.style.backgroundImage= "url("+item2["backdrop_path"]+")"
-        introair.innerHTML=item2["name"] +"("+item2["first_air_date"].substring(0,4)+")"
-        index = (index==4)?0:++index
-    }
+async function handleTrending() {
+  var data = await getJSON("/trending")
+  var dataAir = await getJSON("/airtoday")
+  // trending movies
+  var imgtre = document.getElementById("imgtre")
+  var introtre = document.getElementById("introtre")
+  // air today
+  var imgair = document.getElementById("imgair")
+  var introair = document.getElementById("introair")
+  var index = 0
+  const change = () => {
+    var item = data[index]
+    imgtre.style.backgroundImage = "url(" + item["backdrop_path"] + ")"
+    introtre.innerHTML = item["title"] + "(" + item["release_date"].substring(0, 4) + ")"
+
+    var item2 = dataAir[index]
+    imgair.style.backgroundImage = "url(" + item2["backdrop_path"] + ")"
+    introair.innerHTML = item2["name"] + "(" + item2["first_air_date"].substring(0, 4) + ")"
+    index = (index == 4) ? 0 : ++index
+  }
+  change()
+  setInterval(function () {
     change()
-    setInterval(function() {  
-        change()
-    },5000) //  change pic/per 5s 
+  }, 5000) //  change pic/per 5s 
 }
 
 
 async function getJSON(url) {
+  var retry_time = 0
+  while (retry_time<5) {
     try {
       let response = await fetch(url);
       return await response.json();
     } catch (error) {
-      console.log('Request Failed', error);
-      return []
-    }
+      retry_time +=1
+      console.log("Request Failed, Retrying the" + retry_time +" time...." , error);
+    }  
   }
-
-
-function search(){
-    var nores = document.getElementById("nores")
-    document.getElementById("search_res").style.display="none"
-    nores.style.display="none"
-
-    var kv = document.getElementById("keyword").value
-    var type =  document.getElementById("category").value
-    
-    if(kv==""||type==""){
-      alert("Please fill out this field")
-      return []
-    }
-    url ="/search?type="+type+"&kv="+kv
-    getJSON(url).then(function(resp){
-      
-      if(resp.length == 0){
-        nores.style.display="block"
-      }else{
-        showResult(resp)
-      }
-    })  
+  console.log('Retried more than 5 times, canceled')
+  return [] 
 }
 
 
-function showResult(data){
-  document.getElementById("nores").display="none"
-  document.getElementById("search_res").style.display="block"
-  var one="";
+//  check serach input  and send query 
+function search() {
+  var nores = document.getElementById("nores")
+  document.getElementById("search_res").style.display = "none"
+  nores.style.display = "none"
+
+  var kv = document.getElementById("keyword").value
+  var type = document.getElementById("category").value
+
+  if (kv == "" || type == "") {
+    alert("Please fill out this field")
+    return []
+  }
+  url = "/search?type=" + type + "&kv=" + kv
+  getJSON(url).then(function (resp) {
+
+    if (resp.length == 0) {
+      nores.style.display = "block"
+    } else {
+      showResult(resp)
+    }
+  })
+}
+
+function clearForm() {
+  document.getElementById("keyword").value = "";
+  document.getElementById("category").value = "";
+}
+
+
+//  fetch and display query result
+function showResult(data) {
+  document.getElementById("nores").display = "none"
+  document.getElementById("search_res").style.display = "block"
+  var one = "";
   data.forEach(item => {
-  one += `<div class="res_ele">
+    one += `<div class="res_ele">
     <div class="res_left" style="background-image:url(${item.poster_path})"></div>
     <div class="res_right">
     <div class="stitle">${item.title}</div>
     <div class="year_type">
-    <span clas="syear">${item.release_date.substring(0,4)}</span>
+    <span clas="syear">${item.release_date.substring(0, 4)}</span>
     <span class="stype"> |&nbsp${item.genres}</span>
     </div>
     <div>
-        <span class="grade">&#9733;${item.vote_average/2}/5</span>
+        <span class="grade">&#9733;${item.vote_average / 2}/5</span>
         <span class="votes">${item.vote_count}&nbsp votes</span>
     </div>
     <div class="sreview">${item.overview}</div>
@@ -90,28 +102,113 @@ function showResult(data){
   group.innerHTML = one
 }
 
-function showDetail(id,type){
-  //  1 =tv 2 =movie
 
+
+// following are about display 
+function closeDetail() {
   var detail_all = document.getElementById("detail_all")
   var all_ele = document.getElementById("all_ele")
-  detail_all.style.display="block"
-  all_ele.style.opacity="0.3"
-  all_ele.style.zIndex="-1"
-  all_ele.style.position="fixed"
+  detail_all.style.display = "none"
+  all_ele.style.opacity = "1"
+  all_ele.style.zIndex = "1"
+  all_ele.style.position = "relative"
 }
 
-function closeDetail(){
+function showDetail(id, type) {
+  // display popup 
   var detail_all = document.getElementById("detail_all")
   var all_ele = document.getElementById("all_ele")
-  detail_all.style.display="none"
-  all_ele.style.opacity="1"
-  all_ele.style.zIndex="1"
-  all_ele.style.position="relative"
+
+  document.getElementById("basic_group").innerHTML = ""
+  document.getElementById("cast_container").innerHTML =""
+  document.getElementById("rev_container").innerHTML =""
+
+
+
+  type = (type == 1) ? "tv" : "movie"
+  showBasic(id, type)
+  showCast(id, type)
+  showRewiew(id, type)
+
+  detail_all.style.display = "block"
+  all_ele.style.opacity = "0.3"
+  all_ele.style.zIndex = "-1"
+  all_ele.style.position = "fixed"
+
 }
 
-function clearForm(){
-    document.getElementById("keyword").value="";
-    document.getElementById("category").value="";
-    
+function showBasic(id, type) {
+  url = "/media_basic?media_id=" + id + "&type=" + type
+  getJSON(url).then(
+    function (item) {
+      document.getElementById("big_poster").style.backgroundImage = "url(" + item.backdrop_path + ")"
+      var htmlEle = `
+      <div>
+          <span class="d_title">${item.name}</span>
+          <a href="https://www.themoviedb.org/${type}/${id}" id="d_link">&nbsp;&nbsp;ⓘ</a>
+      </div>
+      <div id="d_year_type">
+          <span id="d_year">${item.year}</span>
+          <span id="d_type"> |&nbsp${item.genres}</span>
+      </div>
+      <div>
+          <span id="d_grade">&#9733;${item.vote_average}/5</span>
+          <span id="d_votes">${item.vote_count}&nbsp votes</span>
+      </div>
+      <div id="d_review">
+      ${item.overview}
+      </div>
+      <div id="lag">Spoken languages: ${item.spoken_languages}</div>
+      `
+      document.getElementById("basic_group").innerHTML = htmlEle
+    }
+  )
 }
+
+function showCast(id, type) {
+  url = "/media_cast?media_id=" + id + "&type=" + type
+  getJSON(url).then(
+    function (resp) {
+      var all_ele = ""
+      resp.forEach(item => {
+        all_ele += `
+        <div class="cast_ele">
+        <div class="cast_img" style="background-image: url(${item.profile_path});"></div>
+        <div class="cast_name">${item.name}</div>
+        <div>AS</div>
+        <div class="character">${item.character}</div>
+        </div>
+        `
+      })
+      document.getElementById("cast_container").innerHTML = all_ele
+    }
+  )
+
+}
+
+function showRewiew(id, type) {
+  url = "/media_review?media_id=" + id + "&type=" + type
+  getJSON(url).then(
+    function (resp) {
+      let all_ele = ""
+      resp.forEach(item => {
+        all_ele += `
+        <div class="rev_ele">
+        <div class="rev_ur">
+            <span class="rev_user">${item.username}</span> on
+            <span class="rev_date">${item.created_at}</span>
+        </div>
+        <div class="rev_rating" style="display:${(item.rating==-1)?'none':'block'}">&#9733;${item.rating}/5</div>
+        <div class="rev_content">${item.content}</div>
+        <div class="line_container">
+            <div class="rev_line"></div>
+        </div>
+        </div>
+        `
+        document.getElementById("rev_container").innerHTML = all_ele
+      })
+    })
+
+}
+
+
